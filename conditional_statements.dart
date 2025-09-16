09e2e95c29fc3866a27c0b398f017a007fae69c6
irarsenus <tech.irarsenal2012@gmail.com>
2025-09-16 14:35:06 +0200

    apply conditional stament in git

---
+++ conditional_statements.dart
@@ -0,0 +1,47 @@
+// conditional statements 
+
+// --- if condition ---
+void main(){
+ const age =1;
+ if(age<18){
+print('you are under age at age below $age');
+ }
+
+//-- if else condition --
+var marks=45;
+if(marks<=49){
+  print(' you have fail the assessment');}
+else{
+  print('you have passed the assessment');
+}
+//-- if else if condition
+
+final marks_rslt= 70;
+
+if (marks_rslt>=90){
+
+  print('the grade is A');
+}
+
+else if (marks_rslt<=80){
+ print('the grade is B');
+}
+else if (marks_rslt<=70){
+ print('the grade is C');
+}
+
+else if (marks_rslt<=60){
+ print('the grade is D');
+}
+ else if (marks_rslt<=50){
+ print('the grade is C');
+}
+else{
+  print('you have failed');
+}
+
+
+}
+
+
+
